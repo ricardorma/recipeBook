2a5e2f56cc198b54f3405b92836ac6c88f2a0a0b
Ricardo <ricardo7897@gmail.com>
2024-09-27 13:14:27 +0200

    Update generate-config.js

--- Frontend/recipeBook-main/scripts/generate-config.js
+++ Frontend/recipeBook-main/scripts/generate-config.js
@@ -3,18 +3,35 @@ const path = require('path');
 
 // Detectar si es producción o desarrollo
 const env = process.env.NODE_ENV || 'development';  // Usar "development" por defecto
+let configFilePath = '';
+let config = {};
 
 // Definir rutas de los archivos de configuración
-const configFilePath = path.resolve(__dirname, `/src/environments/config/config.${env}.json`);
-const outputFilePath = path.resolve(__dirname, '/src/assets/config/config.json');
+if (env === 'development') {
+  configFilePath = path.resolve(__dirname, '../src/environments/config/config.development.json');
 
-// Leer el archivo de configuración base
-let config = JSON.parse(fs.readFileSync(configFilePath, 'utf8'));
+  // Verificar si el archivo de configuración existe
+  if (!fs.existsSync(configFilePath)) {
+    console.error(`Error: El archivo de configuración ${configFilePath} no existe.`);
+    process.exit(1);  // Salir con un código de error
+  }
 
-// Sobrescribir valores con variables de entorno si existen
-config.apiUrl = process.env.API_URL || config.apiUrl;
-config.apiAuth = process.env.API_AUTH || config.apiAuth;
-config.apiUsers = process.env.API_USERS || config.apiUsers;
+  // Leer el archivo de configuración base
+  config = JSON.parse(fs.readFileSync(configFilePath, 'utf8'));
+} else {
+  // En producción, crear el objeto de configuración directamente
+  config.apiUrl = process.env.API_URL;
+  config.apiAuth = process.env.API_AUTH;
+  config.apiUsers = process.env.API_USERS;
+
+  // Verificar que las variables de entorno estén definidas
+  if (!config.apiUrl || !config.apiAuth || !config.apiUsers) {
+    console.error('Error: Las variables de entorno no están definidas en producción.');
+    process.exit(1);  // Salir con un código de error
+  }
+}
+
+const outputFilePath = path.resolve(__dirname, '../src/assets/config/config.json');
 
 // Guardar el archivo final de configuración
 fs.writeFileSync(outputFilePath, JSON.stringify(config, null, 2));
