de21b0446f9945984c1231ffd131ec0a1a0eb2bb
Ricardo <ricardo7897@gmail.com>
2024-09-29 17:45:22 +0200

    update env

---
+++ Frontend/recipeBook-main/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000/recipeBook/',
+    apiAuth: 'http://localhost:3000/',
+    apiUsers: 'http://localhost:3000/users/'
+  }; 
---
+++ Frontend/recipeBook-main/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/recipeBook/',
+  apiAuth: 'http://localhost:3000/',
+  apiUsers: 'http://localhost:3000/users/'
+};
+ 
