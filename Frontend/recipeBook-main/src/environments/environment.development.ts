e0d24aa3b1c2d70139173fac902ad295e75a1ab9
Ricardo <ricardo7897@gmail.com>
2024-09-29 17:44:59 +0200

    update

--- Frontend/recipeBook-main/src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/recipeBook/',
-    apiAuth: 'http://localhost:3000/',
-    apiUsers: 'http://localhost:3000/users/'
-  }; 
