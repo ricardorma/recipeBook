579b4a0e40a5a6b3578598ae8460043b563e3fb8
Ricardo <ricardo7897@gmail.com>
2024-09-30 12:04:46 +0200

    update back y front

--- Backend/index.js
+++ Backend/index.js
@@ -47,40 +47,10 @@ app.use(
 // Inicio express
 app.use(express.json()); 
 app.use(logger('dev'));  
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://recipe-book-navy.vercel.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
-
-app.options("*", (req, res) => {
-  console.log("preflight");
-  if (
-    req.headers.origin === process.env.BASE_FRONT_URL &&
-    allowMethods.includes(req.headers["access-control-request-method"]) &&
-    allowHeaders.includes(req.headers["access-control-request-headers"])
-  ) {
-    console.log("pass");
-    return res.status(204).send();
-  } else {
-    console.log("fail");
-  }});
+app.use(cors({
+  origin: process.env.BASE_FRONT_URL,  // La URL de tu frontend
+  credentials: true  // Permite el envío de cookies
+}));  
 
 app.use(cookieParser());
 app.enable("trust proxy", 1);
