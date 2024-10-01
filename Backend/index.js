f449a7287d78cd3dab5be3f177ffc57b376aefc4
Ricardo <ricardo7897@gmail.com>
2024-10-01 13:40:36 +0200

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -76,6 +76,20 @@ app.use(cors({
   credentials: true  // Permite el envío de cookies
 }));  
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.BASE_FRONT_URL);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.options('*', cors({
+  origin: process.env.BASE_FRONT_URL,
+  credentials: true
+}));
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 
